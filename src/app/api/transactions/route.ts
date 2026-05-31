c96c3ba5beceda485d7bc51fec32fa91aed55cb7
Paprem39 <sirichaiprem1993@gmail.com>
2026-06-01 04:39:57 +0700

    edit error @auth/next

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 import { getServerSession }from "next-auth";
 
-import { authOptions }from "../auth/[...nextauth]/route";
+import { authOptions } from "@/auth";
 
 
 // GET
